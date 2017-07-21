0b6911092098d369dcaf61491fbb1576b4e47c59
Ashif Ismail <cyber2maro@gmail.com>
2017-07-21 11:56:29 +0530

    modified package structuring

---
+++ app/src/main/java/me/ashif/sunnahaday/di/Injectable.java
@@ -0,0 +1,9 @@
+package me.ashif.sunnahaday.di;
+
+/**
+ * Created by Ashif on 20/7/17,July,2017
+ * github.com/SheikhZayed
+ */
+
+public interface Injectable {
+}
--- app/src/main/java/me/ashif/sunnahaday/ui/MainActivity.java
+++ app/src/main/java/me/ashif/sunnahaday/ui/MainActivity.java
@@ -1,4 +1,4 @@
-package me.ashif.sunnahaday;
+package me.ashif.sunnahaday.ui;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -6,6 +6,8 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import me.ashif.sunnahaday.R;
 
 public class MainActivity extends AppCompatActivity {
 
